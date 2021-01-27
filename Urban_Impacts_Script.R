14759bcf1f24137712f749996d61c6f1f4387b60
steven-brownlee <sbrownlee@live.ca>
2021-01-27 13:25:07 -0800

    Initial setup commit - script creation, definition, packages.

---
+++ Urban_Impacts_Script.R
@@ -0,0 +1,19 @@
+##
+
+# Urban Impact Project
+# Date: January 27 2021,
+# Author: Steven Brownlee
+# Email: sbrownle@sfu.ca
+
+# Description: Script to manage the downloading of remotely sensed imagery
+# as well as externally derived shapefiles and analyze urban lake footprints
+# through time series analysis. 
+
+##
+
+# 1.) Install packages as needed:
+
+devtools::install_github("16EAGLE/getSpatialData")
+
+
+library(getSpatialData)
